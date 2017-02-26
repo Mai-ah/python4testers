bc21fd6762540b22700b9f3dd337827c3af510c8
milena.zahorska <milena.zahorska@stxnext.pl>
2017-02-26 23:41:19 +0100

    add test_modify_grop

--- fixture/group.py
+++ fixture/group.py
@@ -12,6 +12,13 @@ class GroupHelper:
         self.open_group_page()
         # init group creation
         wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit confirmation
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -22,9 +29,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
 
     def return_to_group_page(self):
         wd = self.app.wd
@@ -37,5 +41,16 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit delation
         wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
+    def modify(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # select the first group
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group)
+        # submit confirmation
+        wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
