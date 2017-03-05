20486d4395c63d61e0b386575c3a39cfee12b82c
milena.zahorska <milena.zahorska@stxnext.pl>
2017-03-05 19:49:24 +0100

    add change_field_value method

--- fixture/group.py
+++ fixture/group.py
@@ -19,16 +19,16 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def return_to_group_page(self):
         wd = self.app.wd
@@ -37,17 +37,19 @@ class GroupHelper:
     def del_first(self):
         wd = self.app.wd
         self.open_group_page()
-        # select the first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit delation
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def modify(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # select the first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
         self.fill_group_form(group)
         # submit confirmation
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,12 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify(Group(name="2", header="3", footer="4"))
+    app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify(Group(name="5"))
     app.session.logout()
