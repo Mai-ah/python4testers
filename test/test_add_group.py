feef5e63541806adf2d9c1ceae4b71c030da6be1
milena.zahorska <milena.zahorska@stxnext.pl>
2017-03-12 21:04:26 +0100

    delete_group assertion

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,6 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="1", header="1", footer="1"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,11 +5,17 @@ from model.group import Group
 def test_modify_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(name="2", header="3", footer="4"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(name="5"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
